b1a643368255c9a99c492e7d7242bb692fb3a52f
David Fournie <drfournie@gmail.com>
2021-01-15 18:14:09 -0600

    added comments and updated readme

--- Employee_SQL/queries.sql
+++ Employee_SQL/queries.sql
@@ -1,3 +1,6 @@
+--Create employee salaries view which contains employees' first name,
+-- last name, employee number, sex and salary.
+--view is a join between Employees table and Salaries table
 CREATE VIEW employee_salaries AS
 SELECT Employees.employee_number, Employees.last_name,Employees.first_name,Employees.sex,Salaries.salary
 FROM Employees
@@ -6,6 +9,8 @@ Salaries.employee_number=Employees.employee_number;
 
 SELECT * FROM employee_salaries;
 
+-- Create view of employees that were hired in 1986 that contains first name,
+--last name, and hire date
 CREATE VIEW employees_1986 AS
 SELECT first_name,last_name,hire_date 
 FROM Employees
@@ -13,6 +18,9 @@ WHERE hire_date LIKE '%1986';
 
 SELECT * FROM employees_1986;
 
+--Create view manager_personal_info to be joined with
+-- manager_department_info. View contains employee_number,
+-- last_name, and first_name
 CREATE VIEW manager_personal_info AS
 SELECT employee_number, last_name, first_name
 FROM Employees
@@ -21,7 +29,9 @@ WHERE employee_number IN
 	 FROM Department_Manager
 	 );
 
-
+--Create view manager_departments_info to be joined with
+-- manager_personal_info. View contains employee_number,
+-- dept_name, and dept_number
 CREATE VIEW manager_departments_info AS
 SELECT employee_number,dept_number, 
 	(SELECT Departments.dept_name
@@ -29,6 +39,8 @@ SELECT employee_number,dept_number,
 		WHERE Department_Manager.dept_number= Departments.dept_number)
 FROM Department_Manager;
 
+-- Create full_manager_info_view that is a join of:
+-- manager_personal_info and manager_department_info.
 CREATE VIEW full_manager_info AS
 SELECT manager_personal_info.employee_number, manager_personal_info.first_name,
 		manager_personal_info.last_name, manager_departments_info.dept_number,
@@ -41,6 +53,7 @@ SELECT * FROM full_manager_info;
 
 
 
+--repeat process for non manager employees
 CREATE VIEW employee_personal_info AS
 SELECT employee_number, last_name, first_name
 FROM Employees
@@ -68,17 +81,23 @@ employee_personal_info.employee_number = employee_departments_info.employee_numb
 SELECT * FROM full_employee_info;
 
 
-
+--Create hercules_info view that contains:
+-- first name, last name and sex for employees Whose:
+--first name is Hercules and last name begins with B.
+CREATE VIEW hercules_info AS
 SELECT first_name,last_name,sex
 FROM EMPLOYEES
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 
-
+-- Create view sales_dept_info that contains:
+-- all information from emloyee_departments_info where dept_name is 'Sales'
 CREATE VIEW sales_dept_info AS
 SELECT * FROM employee_departments_info
 WHERE dept_name='Sales';
 
+-- Create view employee_sales_dept_info
+-- used to query join between Employees and sales_dept_info
 CREATE VIEW employee_sales_dept_info AS
 SELECT Employees.employee_number, Employees.first_name, Employees.last_name, sales_dept_info.dept_name
 FROM Employees
@@ -89,12 +108,11 @@ SELECT * FROM employee_departments_info;
 
 
 
+--repeat process for Sales & Development
 CREATE VIEW sales_development_dept_info AS
 SELECT * FROM employee_departments_info
 WHERE dept_name IN ('Sales','Development');
 		
-
-
 CREATE VIEW employee_sales_development_dept_info AS
 SELECT Employees.employee_number, Employees.first_name, Employees.last_name, 
 sales_development_dept_info.dept_name
@@ -105,7 +123,8 @@ Employees.employee_number = sales_development_dept_info.employee_number;
 SELECT * FROM employee_sales_development_dept_info;
 
 
-
+-- Create last_name view count that contains the frequency of
+-- the last names of employees
 CREATE VIEW last_name_count AS
 SELECT last_name, COUNT(last_name) AS "last_name_total"
 FROM Employees
